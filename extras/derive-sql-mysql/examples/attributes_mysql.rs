c2f4d3d34243209ba84725e34ff5208bb4e70fb9
Julien de Charentenay, PhD <julien.decharentenay@gmail.com>
2024-02-04 16:24:40 +0000

    Add support for MySQL

---
+++ extras/derive-sql-mysql/examples/attributes_mysql.rs
@@ -0,0 +1,73 @@
+//! Example demonstrating container and field attributes
+//! Run with `cargo run --example attributes --features mysql`
+//!
+use derive_sql::{Sqlable, SimpleFilter, SimpleLimit};
+
+#[derive(Debug)]
+#[derive(derive_sql_mysql::DeriveMysql)]
+#[derive_sqlite(ident = PersonSql)]        // Use the nominated name for the MySQL wrapper structure (default: PersonMysql)
+#[derive_sqlite(table_name = "person_v1")] // Use the nominated name for the MySQL table (default: person)
+struct Person {
+  #[derive_sqlite(is_primary_key = true)] // Nominate the primary key (duplicates are not allowed)
+  i: u32,         // Note: can't use String as primary key... Needs a set number of characters
+  name: String,
+  #[derive_sqlite(on_insert = set_age_on_insert)] // Nomimate a function to be run when inserting
+  #[derive_sqlite(on_update = set_age_on_update)] // Nomimate a function to be run when updating
+  age: u32,
+  active: bool,
+}
+
+fn set_age_on_insert() -> u32 { 33 }
+fn set_age_on_update() -> u32 { 26 }
+
+fn main() {
+  let conn = mysql::Conn::new(
+    mysql::Opts::from_url("mysql://test@localhost/simpledb").unwrap()
+  ).unwrap();
+  if let Err(e) = sample(conn) {
+    println!("simple::attributes failed with error: {}", e);
+    std::process::exit(1);
+  }
+}
+
+fn sample(conn: mysql::Conn) -> Result<(), Box<dyn std::error::Error>> {
+  let mut db: PersonSql = conn.into();
+
+  // Create Table in MySQL database
+  println!("Create table");
+  db.create_table()?;
+
+  // Insert person into database
+  println!("Insert person...");
+  let person = Person { i: 1, name: "Jo".to_string(), age: 44, active: true };
+  let person = db.insert(person)?;
+  assert!(person.name.eq("Jo"));
+  println!("Insert person... ok");
+
+  // Inserting another person with the same name (ie primary key) should fail
+  println!("Insert person with duplicated name [primary key] fails...");
+  assert!(db.insert(Person { i: 1, name: "Jo".to_string(), age: 32, active: true }).is_err());
+  println!("Insert person with duplicated name [primary key] fails... ok");
+
+  // Check the run of on_insert function
+  println!("Age is assigned when inserting...");
+  let _ = db.insert(Person { i: 2, name: "Jack".to_string(), age: 20, active: true})?;
+  let persons: Vec<Person> = db.select(Box::new(SimpleFilter::try_from(("name", "Jack"))?.and(Box::new(SimpleLimit::try_from(1)?))))?;
+  assert!(persons[0].age == 33);
+  println!("Age is assigned when inserting... ok");
+
+  // Check the 
+  println!("Age is assigned when updating...");
+  db.update(Box::new(SimpleFilter::try_from(("name", "Jack"))?.and(Box::new(SimpleLimit::try_from(1)?))), Person { i: 3, name: "Jack".to_string(), age: 44, active: true })?;
+  let persons: Vec<Person> = db.select(Box::new(SimpleFilter::try_from(("name", "Jack"))?.and(Box::new(SimpleLimit::try_from(1)?))))?;
+  assert!(persons[0].age == 26);
+  println!("Age is assigned when updating... ok");
+
+  // Delete the table
+  println!("Delete table");
+  db.delete_table()?;
+
+  println!("Example `attributes` ran successfully");
+  Ok(())
+}
+
---
+++ extras/derive-sql-mysql/src/lib.rs
@@ -0,0 +1,98 @@
+//! Procedural macro to automatically generate an `Sqlable` trait for the 
+//! provided struct that is compatible with an MySQL database connected using the `mysql` crate
+//! <https://crates.io/crates/mysql>.
+//!
+//! # How to use
+//! 
+//! You write:
+//! ```rust
+//! # use derive_sql::*;
+//! # use derive_sql_mysql::DeriveMySql;
+//! #[derive(DeriveMySql)]
+//! pub struct Person {
+//!   name: String,
+//!   age: u32,
+//! }
+//! ```
+//! 
+//! And you can use:
+//! ```rust
+//! # use derive_sql::*;
+//! # use derive_sql_mysql::DeriveMySql;
+//! # #[derive(DeriveMySql)]
+//! # pub struct Person {
+//! #   name: String,
+//! #   age: u32,
+//! # }
+//!
+//! let pool = mysql::Pool::new("mysql::/localhost/persondb").unwrap();
+//! let mut connection = pool.get_conn()?;
+//! let mut db: PersonMysql = connection.into();
+//!
+//! // initialise
+//! db.create_table().unwrap();
+//!
+//! // Insert entries
+//! db.insert(Person {name: "Abi".to_string(), age: 31 }).unwrap();
+//! db.insert(Person {name: "Bert".to_string(), age: 32 }).unwrap();
+//! db.insert(Person {name: "Charlie".to_string(), age: 33 }).unwrap();
+//! 
+//! // Query
+//! let persons: Vec<Person> = db.select(Box::new(SimpleFilter::try_from(("age", 32)).unwrap())).unwrap();
+//! assert!(persons[0].name.eq("Bert"));
+//!
+//! // Update
+//! db.update(Box::new(SimpleFilter::try_from(("name", "Abi")).unwrap()), Person { name: "Abi".to_string(), age: 32 }).unwrap();
+//!
+//! // Delete
+//! db.delete(Box::new(SimpleFilter::try_from(("name", "Abi")).unwrap())).unwrap();
+//!
+//! // Clear the table
+//! db.delete_table().unwrap();
+//! ```
+//!
+//! # Container attributes:
+//! - `#[derive_sqlite(ident = ...)]` overwrite the name of the `rusqlite` wrapper from `{class}Mysql`;
+//! - `#[derive_sqlite(table_name = "...")]` specify the name of the table (default to the container name in lower case);
+//!
+//! # Field attributes:
+//! - `#[derive_sqlite(is_primary_key = true)]` nominate that one of the field is a primary key. Only one primary key can be specified.
+//! primary key fields are unique in the table. WARNING/LIMITATION: !!! primary key can NOT be a String !!!!
+//! - `#[derive_sqlite(on_insert = ...)]` nominate a function of the type `fn() -> {type}` with `{type}` corresponding to the type of the 
+//! field. The function is called when the item is inserted and the value returned by the function is assigned to the field before the
+//! item is inserted. Typical use is to assign a creation date.
+//! - `#[derive_sqlite(on_update = ...)]` nominate a function of the type `fn() -> {type}` with `{type}` corresponding to the type of the 
+//! field. The function is called when the item is updated and the value returned by the function is assigned to the field before the
+//! item is updated. Typical use is to assign a last modified date.
+//!
+
+mod mysql;
+
+use attribute_derive::{Attribute};
+
+#[derive(Attribute)]
+#[attribute(ident = derive_sqlite)]
+struct Attrs {
+  ident: Option<syn::Ident>,
+  table_name: Option<String>,
+}
+
+#[derive(Attribute)]
+#[attribute(ident = derive_sqlite)]
+struct FieldAttrs {
+  #[attribute(default = false)]
+  is_primary_key: bool,
+  on_insert: Option<syn::PatPath>,
+  on_update: Option<syn::PatPath>,
+}
+
+#[proc_macro_derive(DeriveMysql, attributes(derive_sqlite))]
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+  syn::parse(input)
+  .and_then(|ast: syn::DeriveInput| {
+    Ok(mysql::Mysql::try_from(&ast)?.generate()?)
+  })
+  .unwrap_or_else(|e| e.into_compile_error())
+  .into()
+}
+
